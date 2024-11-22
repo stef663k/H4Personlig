a49dd6cb308f5696f394b67814470d37ccfb68a8
stef663k <stef663k@elev.techcollege.dk>
2024-11-22 13:53:55 +0100

    Initialize project structure for BigData application with mapper and reducer components

---
+++ BigData/Producer/Program.cs
@@ -0,0 +1,106 @@
+﻿﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Kafka.Public;
+using Kafka.Public.Loggers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace KafkaDemo
+{
+    public static class KafkaSettings
+    {
+        public const string Topic = "BB";
+        public const string BootstrapServers = "172.16.250.13:9092";
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, collection) =>
+                {
+                    collection.AddHostedService<KafkaConsumerHostedService>();
+                    collection.AddHostedService<KafkaProducerHostedService>();
+                });
+    }
+    
+    
+
+    public class KafkaConsumerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaConsumerHostedService> _logger;
+        private readonly ClusterClient _cluster;
+
+        public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+        {
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration
+            {
+                Seeds = KafkaSettings.BootstrapServers
+            }, new ConsoleLogger());
+        }
+        
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cluster.ConsumeFromLatest(KafkaSettings.Topic);
+            _cluster.MessageReceived += record =>
+            {
+                _logger.LogInformation($"Received: {Encoding.UTF8.GetString(record.Value as byte[])}");
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cluster?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+
+    public class KafkaProducerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedService> _logger;
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = KafkaSettings.BootstrapServers
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+        
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < 100; ++i)
+            {
+                var value = $"Hello BB {i}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync(KafkaSettings.Topic, new Message<Null, string>()
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+
+            _producer.Flush(TimeSpan.FromSeconds(10));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BigData/mapper/Program.cs
@@ -0,0 +1,25 @@
+﻿using Sysstem;
+using System.Text.RegularExpressions;
+
+namespace mapper;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string line;
+
+        while((line = Console.ReadLine()) != null)
+        {
+            var onlyText = Regex.Replace(line, @"\.|;|;|,|[0-9]'", "");
+
+            var words = Regex.Matches(onlyText, @"[\w]+");
+
+            foreach(var word in words)
+            {
+                Console.WriteLine("{0}\t1", word);
+            }
+        }
+    }
+
+}
